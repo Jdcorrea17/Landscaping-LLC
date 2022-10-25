0893bf93b50113a4cfe7c26c048b8d5f100904f0
Joshua Correa <jdcorrea17@gmail.com>
2022-10-24 22:51:51 -0500

    website style added

---
+++ javascript/homepage.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+require('dotenv').config()
+
+const {PORT} = process.env
+
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, './homepage.html'))
+})
+
+app.post('/seed', seed)
+
+
+app.use(express.static(path.join(__dirname, '../public')))
+
+
+app.listen(PORT, () => {console.log(`Listening on port 4005`)})
