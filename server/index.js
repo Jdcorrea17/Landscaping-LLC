3680e7c25398af1720a7800d8f00f37eba24a9a9
Joshua Correa <jdcorrea17@gmail.com>
2022-10-25 17:49:29 -0500

    bug

--- server/index.js
+++ server/index.js
@@ -21,4 +21,4 @@ app.get('/', function(req, res){
 app.use(express.static(path.join(__dirname, './public')))
 
 
-app.listen(PORT, () => {console.log(`Listening on port 5432`)})
+app.listen(PORT, () => {console.log(`Listening on port 4005`)})
